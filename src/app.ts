610fe3ae81f29899e8c2a151ed96f96423886f02
FernandoIllanes <lpze.alvarofernando.illanes.ca@unifranz.edu.bo>
2024-03-11 16:25:17 -0400

    funciona con numeros de teléfono de extension +

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,49 @@
-import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from '@bot-whatsapp/bot'
-import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
+import {
+  createBot,
+  createFlow,
+  MemoryDB,
+  createProvider,
+  addKeyword,
+} from "@bot-whatsapp/bot";
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys";
 
-const flowBienvenida = addKeyword('Hola').addAnswer('Datos filtrados subidos a Google Sheets')
-
-const main = async () => {
-    
-    const provider = createProvider(BaileysProvider)
-
-    provider.initHttpServer(3002)
-    
-    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
-        await bot.sendMessage('59169973651', 'Datos filtrados subidos a Google Sheets', {})
-        res.end('Esto es del server')
-    }))
-
-    await createBot({
-        flow: createFlow([flowBienvenida]),
-        database: new MemoryDB(),
-        provider
-    })
+const flowBienvenida = addKeyword("Hola").addAnswer("Datos funcionales");
+// Función para realizar el reemplazo de variables
+function replaceVariables(template, values) {
+  return template.replace(/{([^{}]*)}/g, (match, key) => {
+    return values[key] || match;
+  });
 }
 
-main()
+const main = async () => {
+  const provider = createProvider(BaileysProvider);
+
+  provider.initHttpServer(3002);
+
+  provider.http?.server.post(
+    "/send-message",
+    handleCtx(async (bot, req, res) => {
+      const cleanedNumber = req.body.number.replace(/\+/g, "");
+
+      if (req.body.message_type === "static") {
+        await bot.sendMessage(cleanedNumber, req.body.message, {});
+      } else if (req.body.message_type === "customized") {
+        // Reemplazar variables en el message_template
+        const message = replaceVariables(req.body.message_template, req.body);
+        await bot.sendMessage(cleanedNumber, message, {});
+      } else {
+        console.log("Tipo de mensaje no válido");
+      }
+
+      res.end("Esto es del server");
+    })
+  );
+
+  await createBot({
+    flow: createFlow([flowBienvenida]),
+    database: new MemoryDB(),
+    provider,
+  });
+};
+
+main();
